0292a91970aaeba85254cea8e6201944b6c4e6b8
bitcamp\AIA <agatha2003@naver.com>
2023-04-18 18:08:50 +0900

    오타 변경

--- day16/src/exception/custom/Account.java
+++ day16/src/exception/custom/Account.java
@@ -1,25 +1,24 @@
 package exception.custom;
 
 public class Account {
-	
+
 	private long balance;
-	
+
 	public long getBalance() {
 		return this.balance;
 	}
-	
-	public void deposit(int money) {
+
+	public void deposit(int money) throws DepositMinusMoneyException {
 		if(money < 0) {
-			System.out.println(new StringBuffer("음수로 입금 불가"));
+			throw new DepositMinusMoneyException("음수로 입금할 수 없습니다.");
 		}
 		this.balance += money;
 	}
-	
+
 	public void withdraw(int money) throws BalanceInsufficientException{
 		if(this.balance < money) {
-			System.out.println(new StringBuffer("잔고부족"));
+			throw new BalanceInsufficientException("잔고부족");
 		}
 		this.balance -= money;
 	}
-	
 }
--- day16/src/exception/custom/MainClass.java
+++ day16/src/exception/custom/MainClass.java
@@ -4,20 +4,29 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		Account acc = new Account();
-		acc.deposit(100000);
-		System.out.println(new StringBuffer("입금후 잔액: ").append(acc.getBalance()).append("원"));
-		
+
 		try {
+			acc.deposit(100000);
+			System.out.println(new StringBuffer("입금후 잔액 : ")
+								.append(acc.getBalance())
+								.append("원"));
 			acc.withdraw(1000000);
-		}catch(BalanceInsufficientException e) {
+		} catch (BalanceInsufficientException e) {
 			/*
 			 * 예외 클래스가 제공하는 getMessage() 메서드는
-			 * 예외의 원인 메시지를 String 타입으로 리턴합니다.
-			 * 자바 표준 API에서 제공하는 다양한 예외 클래스들은
-			 * 각각의 예외 원인 메시지가 기본적으로 객체안에 저장 되어 있습니다.
+			 * 예외의 원인 메시지를 String타입으로 리턴합니다.
+			 * 자바 표준 API에서 제공하는 다양한 예외클래스들은
+			 * 각각의 예외 원인 메시지가 기본적으로 객체 안에
+			 * 저장되어 있습니다.
 			 */
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		} catch (DepositMinusMoneyException e) {
+			e.printStackTrace();
 			System.err.println(e.getMessage());
 		}
-		System.out.println(new StringBuffer("출금 후 잔액: ").append(acc.getBalance()).append("원"));
+		System.out.println(new StringBuffer("출금 후 잔액 : ")
+							.append(acc.getBalance())
+							.append("원"));
 	}
 }
